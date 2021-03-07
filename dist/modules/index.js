144c29abc3acf939fe24caa91a3c5886deec655b
alfie chiong <alfiechiong@gmail.com>
2021-03-07 09:13:33 +0800

    added README

--- dist/modules/index.js
+++ dist/modules/index.js
@@ -1,9 +1,10 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -37,8 +38,8 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var _this = this;
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.loginUser = exports.authToken = void 0;
 var jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 var dotenv_1 = __importDefault(require("dotenv"));
 var auth_1 = __importDefault(require("./auth"));
@@ -64,7 +65,7 @@ exports.authToken = (function (req, res, next) {
 var generateAccessToken = function (loginData) {
     return jsonwebtoken_1.default.sign(loginData, process.env.SECRET_KEY, { expiresIn: '1800s' });
 };
-exports.loginUser = function (loginData) { return __awaiter(_this, void 0, void 0, function () {
+var loginUser = function (loginData) { return __awaiter(void 0, void 0, void 0, function () {
     var authenticated;
     return __generator(this, function (_a) {
         switch (_a.label) {
@@ -81,3 +82,4 @@ exports.loginUser = function (loginData) { return __awaiter(_this, void 0, void 
         }
     });
 }); };
+exports.loginUser = loginUser;
